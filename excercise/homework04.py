69a73ac5fd96c8a9bc98081cdf44235596dd3806
Hung Le <levanhung2611@gmail.com>
2023-11-08 16:23:22 +0700

    exercise for logic practice at home, deadline: 18h30 Friday, 10.11.2023

---
+++ excercise/homework04.py
@@ -0,0 +1,23 @@
+"""
+Toán lớp 7:
+để tổ chức liên hoan cho gia đình, mẹ bảo An đi chợ mua tôm, bò và tôm sú. Mẹ có một vài yêu cầu như sau:
+  - tổng số cân nặng của 3 loại (tôm, bò, heo) là 2.9 ki-lô-gam.
+  - tiền chi mua mỗi loại (tôm, bò, heo) là như nhau
+Được biết: 
+  - giá thịt bò: 280vnd/kg
+  - giá thịt heo: 160vnd/kg
+  - giá tôm sú: 320vnd/kg
+hỏi mỗi loại thực phẩm An mua bao nhiêu ki-lô-gam?
+
+Input:
+  giá thực phẩm 1: [price]
+  giá thực phẩm 2: [price]
+  giá thực phẩm 3: [price]
+  tổng cân nặng: [weight]
+  
+Output:
+  Có thể mua được:
+    {weight} ki-lô-gam {tên thực phẩm 1}
+    {weight} ki-lô-gam {tên thực phẩm 2}
+    {weight} ki-lô-gam {tên thực phẩm 3}
+"""
