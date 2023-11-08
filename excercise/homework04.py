4fa90f765f67576bd275ca535ea7f8d965fbbf87
Hung Le <levanhung2611@gmail.com>
2023-11-08 16:29:45 +0700

    update homework04.py

--- excercise/homework04.py
+++ excercise/homework04.py
@@ -1,20 +1,24 @@
 """
-Toán lớp 7:
-để tổ chức liên hoan cho gia đình, mẹ bảo An đi chợ mua tôm, bò và tôm sú. Mẹ có một vài yêu cầu như sau:
+TOÁN TIỂU HỌC:
+
+Để tổ chức liên hoan cho gia đình, mẹ nhờ Nguyễn Trần Vĩnh An đi chợ mua heo, bò và tôm sú. Mẹ có một vài yêu cầu như sau:
   - tổng số cân nặng của 3 loại (tôm, bò, heo) là 2.9 ki-lô-gam.
   - tiền chi mua mỗi loại (tôm, bò, heo) là như nhau
+
 Được biết: 
   - giá thịt bò: 280vnd/kg
   - giá thịt heo: 160vnd/kg
   - giá tôm sú: 320vnd/kg
-hỏi mỗi loại thực phẩm An mua bao nhiêu ki-lô-gam?
+
+Hỏi mỗi loại thực phẩm thằng An mua được bao nhiêu ki-lô-gam?
+ ----------------------------------------------------------
 
 Input:
   giá thực phẩm 1: [price]
   giá thực phẩm 2: [price]
   giá thực phẩm 3: [price]
   tổng cân nặng: [weight]
-  
+
 Output:
   Có thể mua được:
     {weight} ki-lô-gam {tên thực phẩm 1}
