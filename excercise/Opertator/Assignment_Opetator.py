b77012a522c0327616feb943d3018f87c8d91dff
Hung Le <levanhung2611@gmail.com>
2023-11-20 17:01:52 +0700

    new excercise

---
+++ excercise/Opertator/Assignment_Opetator.py
@@ -0,0 +1,5 @@
+"""
+Toán tử Gán:
+  1. Tạo hai biến a và b, sau đó hoán đổi giá trị của chúng mà không sử dụng biến thứ ba.
+  2. Sử dụng toán tử gán để cập nhật một biến số nguyên với giá trị mới là bản nhân của giá trị hiện tại với 2.
+"""
