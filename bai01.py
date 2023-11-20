f8cfbe883e123ce15305ccd4319d4789961a1433
DzinhEnn <vinhan1410@gmail.com>
2023-11-20 17:14:10 +0700

    new commit for excercise

--- bai01.py
+++ bai01.py
@@ -1,1 +1,19 @@
-print("Hello World")
+class myClass():
+    def menthod1(self):
+        print('Đây là lớp cơ sở')
+
+class myClass1():
+    def menthod01(self):
+        print('Đây là lớp cơ sở')
+
+class childClass(myClass1, myClass):
+    def menthod2(self):
+        print('Đây là lớp con')
+
+c2 = childClass()
+c2.menthod01()
+c2.menthod2()
+
+
+
+        
